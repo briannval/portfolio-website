5c86ef32502a67b6b5edec0c22706127b5bd0cee
briannval <brianvalentinoadhitya@gmail.com>
2024-01-19 16:02:19 -0800

    Added zod validation to form on frontend

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,11 +1,15 @@
 import Contact from "@/components/contact";
+import VideoBackground from "@/components/videoBackground";
 
 export default function Page() {
   return (
-    <main className="h-full w-full">
-      <div className="flex flex-col h-[850px]">
-        <Contact />
-      </div>
-    </main>
+    <>
+      <VideoBackground />
+      <main className="h-full w-full">
+        <div className="flex flex-col h-[850px]">
+          <Contact />
+        </div>
+      </main>
+    </>
   );
 }

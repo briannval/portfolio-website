347f9629a7fb122dc82cd85405c96f32ae7aa29b
briannval <brianvalentinoadhitya@gmail.com>
2024-01-19 16:55:43 -0800

    resolved build errors

--- components/projects.tsx
+++ components/projects.tsx
@@ -4,9 +4,7 @@ const Projects = () => {
   return (
     <SecondaryWrapper>
       <div className="h-full w-full flex flex-col gap-10 py-10 px-10 justify-center m-auto text-start">
-        <h1 className="text-white text-6xl font-mono relative">
-          Projects Page
-        </h1>
+        <h1 className="text-white text-6xl font-mono relative"></h1>
       </div>
     </SecondaryWrapper>
   );

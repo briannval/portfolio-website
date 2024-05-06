942e337db78e99eb5aa983c273605f88e8f233b1
brianadhitya <brianvalentinoadhitya@gmail.com>
2024-05-06 16:25:33 +0700

    Minor navbar fix

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -33,21 +33,24 @@ const Navbar = () => {
     <nav className="mb-10 flex relative shadow-lg justify-center text-start">
       <div className="bg-white bg-opacity-10 text-white font-semibold py-2 px-4 border border-white border-opacity-15 rounded-full inline-flex h-15">
         <div className="rounded-full background-transparent border-white flex flex-row items-center justify-between m-auto items-center">
-          {navLinks.map((navLink) => (
-            <a
-              key={navLink.href}
-              href={navLink.href}
-              className="text-white antialiased tracking-normal hover:tracking-wider hover:underline hover:underline-offset-4 text-xs md:text-sm px-5"
-            >
-              <span
-                className={
-                  activePage === navLink.page ? "font-extrabold text-lg" : ""
-                }
+          {navLinks.map((navLink) =>
+            activePage !== navLink.page ? (
+              <a
+                key={navLink.href}
+                href={navLink.href}
+                className="text-white antialiased tracking-normal hover:tracking-wider hover:underline hover:underline-offset-4 text-xs md:text-sm px-5"
               >
                 {navLink.title}
-              </span>{" "}
-            </a>
-          ))}
+              </a>
+            ) : (
+              <p
+                key={navLink.href}
+                className="text-white antialiased tracking-normal text-md md:text-lg px-5 font-black"
+              >
+                {navLink.title}
+              </p>
+            )
+          )}
         </div>
       </div>
     </nav>
